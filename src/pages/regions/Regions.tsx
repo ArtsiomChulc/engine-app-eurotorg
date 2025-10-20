7e725db7149f4884f26e256ce96d6a402001f1d4
ArtsiomChults <artemorsha@mail.ru>
2025-10-20 15:26:55 +0300

    fix: add icon in input

--- src/pages/regions/Regions.tsx
+++ src/pages/regions/Regions.tsx
@@ -1,13 +1,8 @@
-import {
-    regionsPageData
-} from '@/pages/regions/schemas/regionsPageData';
-import {
-    SelectionItem
-} from '@/shared/components/atoms/selectionItem/SelectionItem';
+import { regionsPageData } from '@/pages/regions/schemas/regionsPageData';
+import { SelectionItem } from '@/shared/components/atoms/selectionItem/SelectionItem';
 import { Text } from '@/shared/components/atoms/text/Text';
-import {
-    TextField
-} from '@/shared/components/atoms/textField/TextField';
+import { TextField } from '@/shared/components/atoms/textField/TextField';
+import { CiSearch } from 'react-icons/ci';
 import styled from 'styled-components';
 
 const RegionsStyled = styled.div`
@@ -16,24 +11,30 @@ const RegionsStyled = styled.div`
     > div {
         margin-bottom: 20px;
     }
-`
+`;
 
 const SelectionItemStyled = styled.div`
     width: 100%;
     margin-top: 20px;
-`
+`;
 
 export const Regions = () => {
     return (
         <RegionsStyled>
-            <Text size={'2xl'} weight={'bold'} variant={'h1'} isTitle>Регионы</Text>
-            <TextField placeholder={'Поиск региона'}/>
-            <Text size={'2xl'} weight={'bold'} variant={'h2'} position={'left'}>Все регионы</Text>
+            <Text size={'2xl'} weight={'bold'} variant={'h1'} isTitle>
+                Регионы
+            </Text>
+            <TextField
+                placeholder={'Поиск региона'}
+                icon={<CiSearch color={'var(--text-placeholder)'} size={18} />}
+                isSearch
+            />
+            <Text size={'2xl'} weight={'bold'} variant={'h2'} position={'left'}>
+                Все регионы
+            </Text>
             <SelectionItemStyled>
-                {regionsPageData.map(({to, title, id}) => {
-                    return (
-                        <SelectionItem key={id} to={to} title={title}/>
-                    )
+                {regionsPageData.map(({ to, title, id }) => {
+                    return <SelectionItem key={id} to={to} title={title} />;
                 })}
             </SelectionItemStyled>
         </RegionsStyled>
