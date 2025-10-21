4d766f2779596f5eedebb92ed137ce054e6e59dc
ArtsiomChults <artemorsha@mail.ru>
2025-10-21 15:48:54 +0300

    fix: design

--- src/pages/marketsPage/Markets.tsx
+++ src/pages/marketsPage/Markets.tsx
@@ -1,6 +1,10 @@
 import { MarketsType } from '@/entities/markets/types';
 import { SelectionItem } from '@/shared/components/atoms/selectionItem/SelectionItem';
 import { Text } from '@/shared/components/atoms/text/Text';
+import {
+    TextField
+} from '@/shared/components/atoms/textField/TextField';
+import { CiSearch } from 'react-icons/ci';
 import styled from 'styled-components';
 
 type MarketsChoiceContainerProps = {
@@ -33,6 +37,11 @@ export const Markets = ({ markets }: MarketsChoiceContainerProps) => {
             <Text weight={'bold'} variant={'h1'} isTitle>
                 Объекты
             </Text>
+            <TextField
+                placeholder={'Поиск магазина'}
+                icon={<CiSearch color={'var(--text-placeholder)'} size={18} />}
+                isSearch
+            />
             <Text size={'2xl'} weight={'bold'} variant={'h2'} position={'left'}>
                 Выберите объект
             </Text>
