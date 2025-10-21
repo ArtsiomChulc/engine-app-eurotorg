d29536a81f90e2fae6e1a9b36b864ce7dbb533ee
ArtsiomChults <artemorsha@mail.ru>
2025-10-21 15:49:22 +0300

    add: types

--- src/entities/markets/types.ts
+++ src/entities/markets/types.ts
@@ -2,4 +2,36 @@ export type MarketsType = {
     marketName: string;
     address: string;
     id: string;
+};
+
+export enum HeatingType {
+    ELECTRO = 'электро',
+    CENTRAL = 'центральное',
+    PEAT = 'ТТ',
+    PELLET = 'пеллеты',
+    GAS = 'газ',
+    NO = 'нет',
 }
+
+export enum SewerageType {
+    CENTRAL = 'центральная',
+    SEPTIC_TANK = 'септик',
+    NO = 'нет',
+}
+
+
+type MeterNumbers = {
+    id: string;
+    nomination: string;
+    number: string;
+};
+
+export type MarketDetailsType = {
+    id: string;
+    meterNumber: Array<MeterNumbers>;
+    heating: HeatingType;
+    waterSupply: boolean;
+    sewerage: SewerageType;
+    installedCapacity: string;
+    existingCapacity: string;
+};
