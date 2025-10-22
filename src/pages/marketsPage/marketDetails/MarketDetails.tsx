53407e02dfdf15f47fb57bff06e1ade18fcb2621
ArtsiomChults <artemorsha@mail.ru>
2025-10-22 14:19:24 +0300

    add: title -fix style

--- src/pages/marketsPage/marketDetails/MarketDetails.tsx
+++ src/pages/marketsPage/marketDetails/MarketDetails.tsx
@@ -1,21 +1,30 @@
 import { MarketDetailsType } from '@/entities/markets/types';
-import {
-    InfoPieceBlock
-} from '@/shared/components/molecules/infoPieceBlock/InfoPieceBlock';
+import { Text } from '@/shared/components/atoms/text/Text';
+import { InfoPieceBlock } from '@/shared/components/molecules/infoPieceBlock/InfoPieceBlock';
 import styled from 'styled-components';
 
 type MarketDetailsProps = {
-    details: MarketDetailsType
-}
+    details: MarketDetailsType;
+};
 
 const MarketDetailsStyled = styled.div`
-    
-`
+    > h2 {
+        margin-bottom: 20px;
+    }
+`;
 
-export const MarketDetails = ({details}: MarketDetailsProps) => {
+export const MarketDetails = ({ details }: MarketDetailsProps) => {
     return (
         <MarketDetailsStyled>
-            <InfoPieceBlock data={details}/>
+            <Text
+                variant={'h2'}
+                position={'center'}
+                color={'secondary'}
+                size={'xl'}
+            >
+                Информация по ТО №{details.marketNumber}
+            </Text>
+            <InfoPieceBlock data={details} />
         </MarketDetailsStyled>
     );
 };
