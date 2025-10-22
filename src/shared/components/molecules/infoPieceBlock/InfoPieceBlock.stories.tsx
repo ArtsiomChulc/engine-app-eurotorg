5171a843dbd41459dad28d7ae79b95c010b21152
ArtsiomChults <artemorsha@mail.ru>
2025-10-22 15:26:09 +0300

    fix: data

--- src/shared/components/molecules/infoPieceBlock/InfoPieceBlock.stories.tsx
+++ src/shared/components/molecules/infoPieceBlock/InfoPieceBlock.stories.tsx
@@ -1,15 +1,29 @@
+import { Layout } from '@/app/layout/Layout';
+import {
+    MarketDetailsType,
+    HeatingType,
+    SewerageType,
+} from '@/entities/markets/types';
 import { InfoPieceBlock } from '@/shared/components/molecules/infoPieceBlock/InfoPieceBlock';
 import type { Meta, StoryObj } from '@storybook/react-vite';
 import '../../../../index.css';
 
-const mockData = [
-    {id: '1', title: 'Номер учета ЭЭ', text: '029229039393939393393939'},
-    {id: '2', title: 'Отопление', text: 'центральное'},
-    {id: '3', title: 'Водоснабжение', text: 'Да'},
-    {id: '4', title: 'канализация', text: 'Да'},
-    {id: '5', title: 'Установленная мощность', text: '35'},
-    {id: '6', title: 'Существующая нагрузка', text: '20'}
-]
+const mockData: MarketDetailsType = {
+    id: '101010',
+    marketNumber: '803',
+    meterNumber: [
+        {
+            id: '345',
+            nomination: 'eee',
+            number: '459595959',
+        },
+    ],
+    heating: HeatingType.CENTRAL,
+    installedCapacity: '200',
+    waterSupply: true,
+    sewerage: SewerageType.SEPTIC_TANK,
+    existingCapacity: '140',
+};
 
 const meta = {
     component: InfoPieceBlock,
@@ -21,8 +35,18 @@ const meta = {
     argTypes: {},
     decorators: [
         Story => (
-            <div style={{ minWidth: 360, width: '100%' }}>
-                <Story />
+            <div
+                style={{
+                    minWidth: '100%',
+                    height: '100%',
+                    background: 'var(--bg-secondary)',
+                    margin: '0 auto',
+                    padding: 10,
+                }}
+            >
+                <Layout>
+                    <Story />
+                </Layout>
             </div>
         ),
     ],
@@ -33,19 +57,20 @@ type Story = StoryObj<typeof meta>;
 
 export const InfoPieceBlockStory: Story = {
     args: {
-        data: mockData
+        data: mockData,
+        edit: false,
     },
 };
 
 export const InfoPieceBlockNoDataStory: Story = {
     args: {
-        data: []
+        edit: false,
     },
 };
 
 export const InfoPieceBlockIsLoadingStory: Story = {
     args: {
-        data: [],
+        edit: false,
         isLoading: true,
     },
 };
