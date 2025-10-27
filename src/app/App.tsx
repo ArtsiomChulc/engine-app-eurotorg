11b9d810e87e966657a9f5c9750b648f2f0373a0
ArtsiomChults <artemorsha@mail.ru>
2025-10-27 16:31:05 +0300

    add: drawing test

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,51 +1,57 @@
 import '../App.css';
 import { Layout } from '@/app/layout/Layout';
+import { useAppSelector } from '@/app/store/store';
 import {
     HeatingType,
     SewerageType,
     WaterSupply,
 } from '@/entities/markets/types';
+import { selectIsAuthenticated } from '@/features/auth';
 import { AuthPage } from '@/pages/authPage/AuthPage';
-import {
-    MarketDetails
-} from '@/pages/marketsPage/marketDetails/MarketDetails';
+import { MarketDetails } from '@/pages/marketsPage/marketDetails/MarketDetails';
 
 function App() {
+    const isAuthenticated = useAppSelector(selectIsAuthenticated);
+
+    if (!isAuthenticated) {
+        return <AuthPage />;
+    }
+
     return (
         <Layout>
-            {/*<AuthPage />*/}
-            <MarketDetails details={{
-                id: '123',
-                marketNumber: '803',
-                meterNumber: [
-                    {
-                        id: '1234',
-                        nomination: 'электро',
-                        number: '08798273r98273r98273r'
-                    },
-                    {
-                        id: '12344445',
-                        nomination: 'вода',
-                        number: '0879ывсыв8273r98273asdw98273r'
-                    },
-                    {
-                        id: '12344445ascasdc',
-                        nomination: 'тепло',
-                        number: '087982ывс73r98273asdw98273r'
-                    },
-                    {
-                        id: '12344445qqqwwweerrr',
-                        nomination: 'газ',
-                        number: '08798273r9827фывс3asdw98273r'
-                    }
-                ],
-                heating: HeatingType.ELECTRO,
-                waterSupply: WaterSupply.NO,
-                sewerage: SewerageType.CENTRAL,
-                installedCapacity: '120',
-                existingCapacity: '90'
-            }
-            }/>
+            <MarketDetails
+                details={{
+                    id: '123',
+                    marketNumber: '803',
+                    meterNumber: [
+                        {
+                            id: '1234',
+                            nomination: 'электро',
+                            number: '08798273r98273r98273r',
+                        },
+                        {
+                            id: '12344445',
+                            nomination: 'вода',
+                            number: '0879ывсыв8273r98273asdw98273r',
+                        },
+                        {
+                            id: '12344445ascasdc',
+                            nomination: 'тепло',
+                            number: '087982ывс73r98273asdw98273r',
+                        },
+                        {
+                            id: '12344445qqqwwweerrr',
+                            nomination: 'газ',
+                            number: '08798273r9827фывс3asdw98273r',
+                        },
+                    ],
+                    heating: HeatingType.ELECTRO,
+                    waterSupply: WaterSupply.NO,
+                    sewerage: SewerageType.CENTRAL,
+                    installedCapacity: '120',
+                    existingCapacity: '90',
+                }}
+            />
         </Layout>
     );
 }
