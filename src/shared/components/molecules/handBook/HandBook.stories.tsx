177ce9c082c034e68d4a6598d02cd672663e8656
ArtsiomChults <artemorsha@mail.ru>
2025-11-11 23:40:00 +0300

    add: HandBook.tsx

---
+++ src/shared/components/molecules/handBook/HandBook.stories.tsx
@@ -0,0 +1,65 @@
+import { Layout } from '@/app/layout/Layout';
+import { HandBook } from '@/shared/components/molecules/handBook/HandBook';
+import { HandbookType } from '@/typesCommon/markets/types';
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import '../../../../index.css';
+
+const mockData: HandbookType[] = [
+    {
+        id: '1',
+        marketAddress: 'Орша, ул. Хренулица, 38',
+        marketNumber: '555',
+        district: 'Оршанский',
+        responsible: 'Чульц',
+        boss: 'Клява',
+    },
+    {
+        id: '2',
+        marketAddress: 'Орша, ул. Хренулица, 38',
+        marketNumber: '555',
+        district: 'Оршанский',
+        responsible: 'Чульц',
+        boss: 'Клява',
+    },
+    {
+        id: '3',
+        marketAddress: 'Орша, ул. Хренулица, 38',
+        marketNumber: '555',
+        district: 'Оршанский',
+        responsible: 'Чульц',
+        boss: 'Клява',
+    },
+];
+
+const meta = {
+    component: HandBook,
+    title: 'UI/HandBook',
+    tags: ['autodocs'],
+    argTypes: {},
+    decorators: [
+        Story => (
+            <div
+                style={{
+                    minWidth: '100%',
+                    height: '100%',
+                    background: 'var(--bg-secondary)',
+                    margin: '0 auto',
+                    padding: 10,
+                }}
+            >
+                <Layout>
+                    <Story />
+                </Layout>
+            </div>
+        ),
+    ],
+} satisfies Meta<typeof HandBook>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const HandBookStory: Story = {
+    args: {
+        items: mockData,
+    },
+};
---
+++ src/shared/components/molecules/handBook/HandBook.tsx
@@ -0,0 +1,62 @@
+import { Select } from '@/shared/components/atoms/select/Select';
+import { SelectionItem } from '@/shared/components/atoms/selectionItem/SelectionItem';
+import { options } from '@/shared/components/molecules/registerForm/schemas/optionsData';
+import { HandbookType } from '@/typesCommon/markets/types';
+import styled from 'styled-components';
+
+type HandBookProps = {
+    items?: HandbookType[];
+};
+
+const HandBookContainer = styled.div`
+    width: 100%;
+    padding: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    > div:first-child {
+        margin-bottom: 20px;
+        margin-left: auto;
+        margin-right: auto;
+    }
+`;
+
+const ItemsWrapper = styled.div`
+    width: 100%;
+    :not(:last-child) {
+        margin-bottom: 6px;
+    }
+`;
+
+export const HandBook = ({ items }: HandBookProps) => {
+    return (
+        <HandBookContainer>
+            <Select options={options} />
+            <ItemsWrapper>
+                {items &&
+                    items.map(
+                        ({
+                            id,
+                            marketAddress,
+                            marketNumber,
+                            district,
+                            responsible,
+                            boss,
+                        }) => {
+                            return (
+                                <SelectionItem
+                                    key={id}
+                                    responsible={responsible}
+                                    marketAddress={marketAddress}
+                                    district={district}
+                                    boss={boss}
+                                    marketNumber={marketNumber}
+                                />
+                            );
+                        }
+                    )}
+            </ItemsWrapper>
+        </HandBookContainer>
+    );
+};
