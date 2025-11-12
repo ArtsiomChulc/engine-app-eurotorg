fd2328c230bb3d9553b449ad47c8d3d2bd831448
ArtsiomChults <artemorsha@mail.ru>
2025-11-12 15:24:15 +0300

    fix: rename

---
+++ src/pages/handBookPage/HandBookPage.tsx
@@ -0,0 +1,9 @@
+import { HandBook } from '@/shared/components/molecules/handBook/HandBook';
+
+export const HandBookPage = () => {
+    return (
+        <>
+            <HandBook />
+        </>
+    );
+};
