14f794bfd7a4a4bae28e690119aaab442d1e36ef
ArtsiomChults <artemorsha@mail.ru>
2025-11-19 00:09:00 +0300

    add: fetch data for AdminPage

--- src/pages/adminPage/AdminPage.tsx
+++ src/pages/adminPage/AdminPage.tsx
@@ -1,13 +1,24 @@
 import {
-    PageTitle
-} from '@/shared/components/molecules/pageTtitle/PageTitle';
+    useGetAdminDataUsersQuery,
+    useGetAdminDataDirectorsQuery, useGetAdminDataEngineersQuery,
+} from '@/api/admin-api';
+import { Loader } from '@/shared/components/atoms/loader/Loader';
+import { PageTitle } from '@/shared/components/molecules/pageTtitle/PageTitle';
 import { Admin } from '@/shared/components/organizms/admin/Admin';
 
 export const AdminPage = () => {
+    const {data: userData, isLoading} = useGetAdminDataUsersQuery();
+    const {data: directorsData} = useGetAdminDataDirectorsQuery();
+    const {data: engineersData} = useGetAdminDataEngineersQuery();
+    console.log('userData', userData);
+    console.log('directorsData', directorsData);
+    console.log('engineersData', engineersData);
+
+    if (isLoading) return <Loader/>
     return (
         <div>
-            <PageTitle title={'Admin'}/>
-            <Admin/>
+            <PageTitle title={'Admin'} />
+            <Admin />
         </div>
     );
 };
